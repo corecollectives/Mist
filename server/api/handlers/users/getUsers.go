ae30b7bcfcd5e3f3a8d35203317f3cc6237c0383
calc <maheshwarivinayak90@gmail.com>
2025-11-05 23:44:31 +0530

    separated user routes and db

--- server/api/handlers/users/getUsers.go
+++ server/api/handlers/users/getUsers.go
@@ -8,26 +8,11 @@ import (
 )
 
 func (h *Handler) GetUsers(w http.ResponseWriter, r *http.Request) {
-	rows, err := h.DB.Query(`
-		SELECT id, username, email, role, created_at, updated_at
-		FROM users
-		ORDER BY id;
-	`)
+
+	users, err := models.GetAllUsers()
 	if err != nil {
-		handlers.SendResponse(w, http.StatusInternalServerError, false, nil, "Database query failed", err.Error())
+		handlers.SendResponse(w, http.StatusInternalServerError, false, nil, "Failed to retrieve users", err.Error())
 		return
 	}
-	defer rows.Close()
-
-	var users []models.User
-	for rows.Next() {
-		var u models.User
-		if err := rows.Scan(&u.ID, &u.Username, &u.Email, &u.Role, &u.CreatedAt, &u.UpdatedAt); err != nil {
-			handlers.SendResponse(w, http.StatusInternalServerError, false, nil, "Failed to scan row", err.Error())
-			return
-		}
-		users = append(users, u)
-	}
-
 	handlers.SendResponse(w, http.StatusOK, true, users, "Users retrieved successfully", "")
 }

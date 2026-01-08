d57ecae3b4de7c62dd3932c70219ac31e6b01671
07calc <maheshwarivinayak90@gmail.com>
2026-01-08 23:21:49 +0530

    go-git integration

---
+++ server/git/clone.go
@@ -0,0 +1,22 @@
+package git
+
+import (
+	"os"
+
+	"github.com/go-git/go-git/v6"
+	"github.com/go-git/go-git/v6/plumbing"
+)
+
+func CloneRepo(url string, branch string, logFile *os.File, path string) error {
+	_, err := git.PlainClone(path, &git.CloneOptions{
+		URL:           url,
+		Progress:      logFile,
+		ReferenceName: plumbing.NewBranchReferenceName(branch),
+		SingleBranch:  true,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}

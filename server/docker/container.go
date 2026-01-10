a3d0dc009a17c55a9152a19cd50f026d1fe13949
07calc <maheshwarivinayak90@gmail.com>
2026-01-10 18:53:08 +0530

    feat: moby for image and container control

--- server/docker/container.go
+++ server/docker/container.go
@@ -4,10 +4,479 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net/netip"
+	"os"
 	"os/exec"
 	"strings"
 	"time"
+
+	"github.com/corecollectives/mist/models"
+	"github.com/moby/moby/api/types/container"
+	"github.com/moby/moby/api/types/network"
+	"github.com/moby/moby/client"
+	"github.com/rs/zerolog/log"
 )
+
+func StopContainer(containerName string) error {
+	if !ContainerExists(containerName) {
+		return fmt.Errorf("container %s does not exist", containerName)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+	defer cancel()
+	cli, err := client.New(client.FromEnv)
+	if err != nil {
+		return fmt.Errorf("error making moby client: %s", err.Error())
+	}
+	_, err = cli.ContainerStop(ctx, containerName, client.ContainerStopOptions{})
+	if err != nil {
+		if ctx.Err() == context.DeadlineExceeded {
+			return fmt.Errorf("docker stop timed out after 2 minutes for container %s", containerName)
+		}
+		return fmt.Errorf("failed to stop container: %w", err)
+	}
+	return nil
+
+	//legacy exec method
+	//
+	//
+	// cmd := exec.CommandContext(ctx, "docker", "stop", containerName)
+	// if err := cmd.Run(); err != nil {
+	// 	if ctx.Err() == context.DeadlineExceeded {
+	// 		return fmt.Errorf("docker stop timed out after 2 minutes for container %s", containerName)
+	// 	}
+	// 	return fmt.Errorf("failed to stop container: %w", err)
+	// }
+	//
+	// return nil
+}
+
+func StartContainer(containerName string) error {
+	if !ContainerExists(containerName) {
+		return fmt.Errorf("container %s does not exist", containerName)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	defer cancel()
+	cli, err := client.New(client.FromEnv)
+	if err != nil {
+		return fmt.Errorf("error creating moby client: %s", err.Error())
+	}
+	_, err = cli.ContainerStart(ctx, containerName, client.ContainerStartOptions{})
+	if err != nil {
+		if ctx.Err() == context.DeadlineExceeded {
+			return fmt.Errorf("docker start timed out after 1 minute for container %s", containerName)
+		}
+		return fmt.Errorf("failed to start container: %w", err)
+	}
+	return nil
+
+	// legacy exec method
+	//
+	//
+	// cmd := exec.CommandContext(ctx, "docker", "start", containerName)
+	// if err := cmd.Run(); err != nil {
+	// 	if ctx.Err() == context.DeadlineExceeded {
+	// 		return fmt.Errorf("docker start timed out after 1 minute for container %s", containerName)
+	// 	}
+	// 	return fmt.Errorf("failed to start container: %w", err)
+	// }
+	//
+	// return nil
+}
+
+func RestartContainer(containerName string) error {
+	if !ContainerExists(containerName) {
+		return fmt.Errorf("container %s does not exist", containerName)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
+	defer cancel()
+	cli, err := client.New(client.FromEnv)
+	if err != nil {
+		return fmt.Errorf("error creating moby client: %s", err.Error())
+	}
+
+	_, err = cli.ContainerRestart(ctx, containerName, client.ContainerRestartOptions{})
+	if err != nil {
+		if ctx.Err() == context.DeadlineExceeded {
+			return fmt.Errorf("dockre restart timed out after 3 minuts for container %s", containerName)
+		}
+		return fmt.Errorf("failed to restart container: %w", err)
+	}
+	return nil
+
+	// legacy exec method
+	//
+	//
+	// cmd := exec.CommandContext(ctx, "docker", "restart", containerName)
+	// if err := cmd.Run(); err != nil {
+	// 	if ctx.Err() == context.DeadlineExceeded {
+	// 		return fmt.Errorf("docker restart timed out after 3 minutes for container %s", containerName)
+	// 	}
+	// 	return fmt.Errorf("failed to restart container: %w", err)
+	// }
+	//
+	// return nil
+}
+
+func GetContainerLogs(containerName string, tail int) (string, error) {
+	if !ContainerExists(containerName) {
+		return "", fmt.Errorf("container %s does not exist", containerName)
+	}
+
+	tailStr := fmt.Sprintf("%d", tail)
+	cmd := exec.Command("docker", "logs", "--tail", tailStr, containerName)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("failed to get container logs: %w", err)
+	}
+
+	return string(output), nil
+}
+
+func GetContainerName(appName string, appId int64) string {
+	return fmt.Sprintf("app-%d", appId)
+}
+
+func StopRemoveContainer(containerName string, logfile *os.File) error {
+	ifExists := ContainerExists(containerName)
+	if !ifExists {
+		return nil
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+	defer cancel()
+	cli, err := client.New(client.FromEnv)
+	if err != nil {
+		return fmt.Errorf("error creating moby client: %s", err.Error())
+	}
+	_, err = cli.ContainerStop(ctx, containerName, client.ContainerStopOptions{})
+	if err != nil {
+		if ctx.Err() == context.DeadlineExceeded {
+			return fmt.Errorf("docker stop timed out after 2 minutes for container %s", containerName)
+		}
+		return fmt.Errorf("failed to stop container %s: %w", containerName, err)
+	}
+
+	_, err = cli.ContainerRemove(ctx, containerName, client.ContainerRemoveOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to remove container %s: %w", containerName, err)
+	}
+
+	return nil
+
+	// legacy exec method
+	//
+	//
+	// stopCmd := exec.CommandContext(ctx, "docker", "stop", containerName)
+	// stopCmd.Stdout = logfile
+	// stopCmd.Stderr = logfile
+	// if err := stopCmd.Run(); err != nil {
+	// 	if ctx.Err() == context.DeadlineExceeded {
+	// 		return fmt.Errorf("docker stop timed out after 2 minutes for container %s", containerName)
+	// 	}
+	// 	return fmt.Errorf("failed to stop container %s: %w", containerName, err)
+	// }
+	//
+	// removeCmd := exec.CommandContext(ctx, "docker", "rm", containerName)
+	// removeCmd.Stdout = logfile
+	// removeCmd.Stderr = logfile
+	// if err := removeCmd.Run(); err != nil {
+	// 	return fmt.Errorf("failed to remove container %s: %w", containerName, err)
+	// }
+	//
+	// return nil
+}
+func ContainerExists(name string) bool {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	defer cancel()
+	cli, err := client.New(client.FromEnv)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to create docker client")
+		return false
+	}
+	_, err = cli.ContainerInspect(ctx, name, client.ContainerInspectOptions{})
+	if err != nil {
+		log.Error().Msg("container not found")
+		return false
+	}
+	return true
+
+	// legacy exec method
+	//
+	//
+	// cmd := exec.Command("docker", "inspect", name)
+	// output, err := cmd.CombinedOutput()
+	//
+	// if err != nil {
+	// 	if strings.Contains(string(output), "No such object") {
+	// 		return false
+	// 	}
+	// 	return false
+	// }
+	//
+	// return true
+}
+
+func RunContainer(app *models.App, imageTag, containerName string, domains []string, Port int, envVars map[string]string, logfile *os.File) error {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	cli, err := client.New(client.FromEnv)
+	if err != nil {
+		return fmt.Errorf("error creating moby client: %s", err.Error())
+	}
+	var restartPolicy container.RestartPolicyMode
+	switch string(app.RestartPolicy) {
+
+	case string(models.RestartPolicyNo):
+		restartPolicy = container.RestartPolicyDisabled
+	case string(models.RestartPolicyAlways):
+		restartPolicy = container.RestartPolicyAlways
+	case string(models.RestartPolicyOnFailure):
+		restartPolicy = container.RestartPolicyOnFailure
+	case string(models.RestartPolicyUnlessStopped):
+		restartPolicy = container.RestartPolicyUnlessStopped
+	default:
+		restartPolicy = container.RestartPolicyUnlessStopped
+	}
+
+	var volumeBinds []string
+	volumes, err := models.GetVolumesByAppID(app.ID)
+	if err == nil {
+		for _, vol := range volumes {
+			volumeBindArg := fmt.Sprintf("%s:%s", vol.HostPath, vol.ContainerPath)
+			if vol.ReadOnly {
+				volumeBindArg += ":ro"
+			}
+			volumeBinds = append(volumeBinds, volumeBindArg)
+		}
+	}
+
+	var envList []string
+	for key, value := range envVars {
+		envList = append(envList, fmt.Sprintf("%s=%s", key, value))
+	}
+
+	labels := make(map[string]string)
+
+	networkMode := ""
+	exposedPorts := make(network.PortSet)
+	portBindings := make(network.PortMap)
+
+	switch app.AppType {
+	case models.AppTypeWeb:
+		if len(domains) > 0 {
+			networkMode = "traefik-net"
+			labels["traefik.enable"] = "true"
+
+			var hostRules []string
+			for _, domain := range domains {
+				hostRules = append(hostRules, fmt.Sprintf("Host(`%s`)", domain))
+			}
+			hostRule := strings.Join(hostRules, " || ")
+
+			labels[fmt.Sprintf("traefik.http.routers.%s.rule", containerName)] = hostRule
+			labels[fmt.Sprintf("traefik.http.routers.%s.entrypoints", containerName)] = "websecure"
+			labels[fmt.Sprintf("traefik.http.routers.%s.tls", containerName)] = "true"
+			labels[fmt.Sprintf("traefik.http.routers.%s.tls.certresolver", containerName)] = "le"
+			labels[fmt.Sprintf("traefik.http.services.%s.loadbalancer.server.port", containerName)] = fmt.Sprintf("%d", Port)
+
+			labels[fmt.Sprintf("traefik.http.routers.%s-http.rule", containerName)] = hostRule
+			labels[fmt.Sprintf("traefik.http.routers.%s-http.entrypoints", containerName)] = "web"
+			labels[fmt.Sprintf("traefik.http.routers.%s-http.middlewares", containerName)] = fmt.Sprintf("%s-https-redirect", containerName)
+			labels[fmt.Sprintf("traefik.http.middlewares.%s-https-redirect.redirectscheme.scheme", containerName)] = "https"
+		} else {
+			port, err := network.ParsePort(fmt.Sprintf("%d/tcp", Port))
+			if err != nil {
+				return fmt.Errorf("failed to parse port: %w", err)
+			}
+			hostIP, err := netip.ParseAddr("0.0.0.0")
+			if err != nil {
+				return fmt.Errorf("failed to parse host IP: %w", err)
+			}
+			exposedPorts[port] = struct{}{}
+			portBindings[port] = []network.PortBinding{
+				{
+					HostIP:   hostIP,
+					HostPort: fmt.Sprintf("%d", Port),
+				},
+			}
+		}
+
+	case models.AppTypeService:
+		networkMode = "traefik-net"
+
+	case models.AppTypeDatabase:
+		networkMode = "traefik-net"
+
+	default:
+		port, err := network.ParsePort(fmt.Sprintf("%d/tcp", Port))
+		if err != nil {
+			return fmt.Errorf("failed to parse port: %w", err)
+		}
+		hostIP, err := netip.ParseAddr("0.0.0.0")
+		if err != nil {
+			return fmt.Errorf("failed to parse host IP: %w", err)
+		}
+		exposedPorts[port] = struct{}{}
+		portBindings[port] = []network.PortBinding{
+			{
+				HostIP:   hostIP,
+				HostPort: fmt.Sprintf("%d", Port),
+			},
+		}
+	}
+
+	hostConfig := container.HostConfig{
+		RestartPolicy: container.RestartPolicy{
+			Name: restartPolicy,
+		},
+		Resources:    container.Resources{},
+		Binds:        volumeBinds,
+		NetworkMode:  container.NetworkMode(networkMode),
+		PortBindings: portBindings,
+	}
+
+	config := container.Config{
+		Image:        imageTag,
+		Env:          envList,
+		Labels:       labels,
+		ExposedPorts: exposedPorts,
+	}
+
+	if app.CPULimit != nil && *app.CPULimit > 0 {
+		hostConfig.Resources.NanoCPUs = int64(*app.CPULimit * 1e9)
+	}
+	if app.MemoryLimit != nil && *app.MemoryLimit > 0 {
+		hostConfig.Resources.Memory = int64(*app.MemoryLimit) * 1024 * 1024
+	}
+
+	resp, err := cli.ContainerCreate(ctx, client.ContainerCreateOptions{
+		Name:       containerName,
+		Config:     &config,
+		HostConfig: &hostConfig,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create container: %w", err)
+	}
+
+	_, err = cli.ContainerStart(ctx, resp.ID, client.ContainerStartOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to start container: %w", err)
+	}
+
+	return nil
+
+	// legacey exec method
+	//
+	//
+	// runArgs := []string{
+	// 	"run", "-d",
+	// 	"--name", containerName,
+	// }
+
+	// restartPolicy := string(app.RestartPolicy)
+	// if restartPolicy == "" {
+	// 	restartPolicy = "unless-stopped"
+	// }
+	// runArgs = append(runArgs, "--restart", restartPolicy)
+	//
+	// if app.CPULimit != nil && *app.CPULimit > 0 {
+	// 	runArgs = append(runArgs, "--cpus", fmt.Sprintf("%.2f", *app.CPULimit))
+	// }
+	//
+	// if app.MemoryLimit != nil && *app.MemoryLimit > 0 {
+	// 	runArgs = append(runArgs, "-m", fmt.Sprintf("%dm", *app.MemoryLimit))
+	// }
+	//
+	// // Add volumes from the volumes table (user-configurable)
+	// volumes, err := models.GetVolumesByAppID(app.ID)
+	// if err == nil {
+	// 	for _, vol := range volumes {
+	// 		volumeArg := fmt.Sprintf("%s:%s", vol.HostPath, vol.ContainerPath)
+	// 		if vol.ReadOnly {
+	// 			volumeArg += ":ro"
+	// 		}
+	// 		runArgs = append(runArgs, "-v", volumeArg)
+	// 	}
+	// }
+	//
+	// for key, value := range envVars {
+	// 	runArgs = append(runArgs, "-e", fmt.Sprintf("%s=%s", key, value))
+	// }
+	//
+	// switch app.AppType {
+	// case models.AppTypeWeb:
+	// 	if len(domains) > 0 {
+	// 		runArgs = append(runArgs,
+	// 			"--network", "traefik-net",
+	// 			"-l", "traefik.enable=true",
+	// 		)
+	//
+	// 		var hostRules []string
+	// 		for _, domain := range domains {
+	// 			hostRules = append(hostRules, fmt.Sprintf("Host(`%s`)", domain))
+	// 		}
+	// 		hostRule := strings.Join(hostRules, " || ")
+	//
+	// 		runArgs = append(runArgs,
+	// 			"-l", fmt.Sprintf("traefik.http.routers.%s.rule=%s", containerName, hostRule),
+	// 			"-l", fmt.Sprintf("traefik.http.routers.%s.entrypoints=websecure", containerName),
+	// 			"-l", fmt.Sprintf("traefik.http.routers.%s.tls=true", containerName),
+	// 			"-l", fmt.Sprintf("traefik.http.routers.%s.tls.certresolver=le", containerName),
+	// 			"-l", fmt.Sprintf("traefik.http.services.%s.loadbalancer.server.port=%d", containerName, Port),
+	// 		)
+	//
+	// 		runArgs = append(runArgs,
+	//
+	// 			"-l", fmt.Sprintf("traefik.http.routers.%s-http.rule=%s", containerName, hostRule),
+	// 			"-l", fmt.Sprintf("traefik.http.routers.%s-http.entrypoints=web", containerName),
+	// 			"-l", fmt.Sprintf("traefik.http.routers.%s-http.middlewares=%s-https-redirect", containerName, containerName),
+	//
+	// 			"-l", fmt.Sprintf("traefik.http.middlewares.%s-https-redirect.redirectscheme.scheme=https", containerName),
+	// 		)
+	// 	} else {
+	// 		runArgs = append(runArgs,
+	// 			"-p", fmt.Sprintf("%d:%d", Port, Port),
+	// 		)
+	// 	}
+	//
+	// case models.AppTypeService:
+	// 	runArgs = append(runArgs, "--network", "traefik-net")
+	//
+	// case models.AppTypeDatabase:
+	// 	runArgs = append(runArgs, "--network", "traefik-net")
+	//
+	// default:
+	// 	runArgs = append(runArgs,
+	// 		"-p", fmt.Sprintf("%d:%d", Port, Port),
+	// 	)
+	// }
+	//
+	// runArgs = append(runArgs, imageTag)
+	//
+	// // ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	// // defer cancel()
+	//
+	// cmd := exec.CommandContext(ctx, "docker", runArgs...)
+	// cmd.Stdout = logfile
+	// cmd.Stderr = logfile
+	//
+	// if err := cmd.Run(); err != nil {
+	// 	if ctx.Err() == context.DeadlineExceeded {
+	// 		return fmt.Errorf("docker run timed out after 5 minutes")
+	// 	}
+	// 	exitCode := -1
+	// 	if exitErr, ok := err.(*exec.ExitError); ok {
+	// 		exitCode = exitErr.ExitCode()
+	// 	}
+	// 	return fmt.Errorf("docker run failed with exit code %d: %w", exitCode, err)
+	// }
+	//
+	// return nil
+}
 
 type ContainerStatus struct {
 	Name    string `json:"name"`
@@ -80,78 +549,32 @@ func GetContainerStatus(containerName string) (*ContainerStatus, error) {
 	}, nil
 }
 
-func StopContainer(containerName string) error {
+func RecreateContainer(app *models.App) error {
+	containerName := GetContainerName(app.Name, app.ID)
+
 	if !ContainerExists(containerName) {
 		return fmt.Errorf("container %s does not exist", containerName)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
-	defer cancel()
-
-	cmd := exec.CommandContext(ctx, "docker", "stop", containerName)
-	if err := cmd.Run(); err != nil {
-		if ctx.Err() == context.DeadlineExceeded {
-			return fmt.Errorf("docker stop timed out after 2 minutes for container %s", containerName)
-		}
-		return fmt.Errorf("failed to stop container: %w", err)
-	}
-
-	return nil
-}
-
-func StartContainer(containerName string) error {
-	if !ContainerExists(containerName) {
-		return fmt.Errorf("container %s does not exist", containerName)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
-	defer cancel()
-
-	cmd := exec.CommandContext(ctx, "docker", "start", containerName)
-	if err := cmd.Run(); err != nil {
-		if ctx.Err() == context.DeadlineExceeded {
-			return fmt.Errorf("docker start timed out after 1 minute for container %s", containerName)
-		}
-		return fmt.Errorf("failed to start container: %w", err)
-	}
-
-	return nil
-}
-
-func RestartContainer(containerName string) error {
-	if !ContainerExists(containerName) {
-		return fmt.Errorf("container %s does not exist", containerName)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
-	defer cancel()
-
-	cmd := exec.CommandContext(ctx, "docker", "restart", containerName)
-	if err := cmd.Run(); err != nil {
-		if ctx.Err() == context.DeadlineExceeded {
-			return fmt.Errorf("docker restart timed out after 3 minutes for container %s", containerName)
-		}
-		return fmt.Errorf("failed to restart container: %w", err)
-	}
-
-	return nil
-}
-
-func GetContainerLogs(containerName string, tail int) (string, error) {
-	if !ContainerExists(containerName) {
-		return "", fmt.Errorf("container %s does not exist", containerName)
-	}
-
-	tailStr := fmt.Sprintf("%d", tail)
-	cmd := exec.Command("docker", "logs", "--tail", tailStr, containerName)
-	output, err := cmd.CombinedOutput()
+	cmd := exec.Command("docker", "inspect", containerName, "--format", "{{.Config.Image}}")
+	output, err := cmd.Output()
 	if err != nil {
-		return "", fmt.Errorf("failed to get container logs: %w", err)
+		return fmt.Errorf("failed to get container image: %w", err)
+	}
+	imageTag := strings.TrimSpace(string(output))
+
+	port, domains, envVars, err := GetDeploymentConfigForApp(app)
+	if err != nil {
+		return fmt.Errorf("failed to get deployment configuration: %w", err)
 	}
 
-	return string(output), nil
-}
+	if err := StopRemoveContainer(containerName, nil); err != nil {
+		return fmt.Errorf("failed to stop/remove container: %w", err)
+	}
 
-func GetContainerName(appName string, appId int64) string {
-	return fmt.Sprintf("app-%d", appId)
+	if err := RunContainer(app, imageTag, containerName, domains, port, envVars, nil); err != nil {
+		return fmt.Errorf("failed to run container: %w", err)
+	}
+
+	return nil
 }

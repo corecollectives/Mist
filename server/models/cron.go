c7203d855086498650b556547be22cbfe425edde
Tanish <mirajkartanish@gmail.com>
2026-01-13 21:44:02 +0530

    rewritten backup cron and deployment files

--- server/models/cron.go
+++ server/models/cron.go
@@ -3,13 +3,13 @@ package models
 import "time"
 
 type Cron struct {
-	ID        int64     `json:"id"`
-	AppID     int64     `json:"app_id"`
-	Name      string    `json:"name"`
-	Schedule  string    `json:"schedule"`
-	Command   string    `json:"command"`
-	LastRun   time.Time `json:"last_run"`
-	NextRun   time.Time `json:"next_run"`
-	Enable    bool      `json:"enable"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        int64     `gorm:"primaryKey;autoIncrement:true" json:"id"`
+	AppID     int64     `gorm:"index;constraint:OnDelete:CASCADE;not null" json:"app_id"`
+	Name      string    `gorm:"index;not null" json:"name"`
+	Schedule  string    `gorm:"not null" json:"schedule"`
+	Command   string    `gorm:"not null" json:"command"`
+	LastRun   *time.Time `gorm:"type:timestamp" json:"last_run"`
+	NextRun   *time.Time `gorm:"type:timestamp" json:"next_run"`
+	Enable    bool      `gorm:"default:true" json:"enable"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 }
